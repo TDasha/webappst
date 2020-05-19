761634bebca25312b6cbe004a3d77abb5b74163c
TDasha <dashakatka97@gmail.com>
2020-05-20 01:42:29 +0600

    Update webb.py

--- webb.py
+++ webb.py
@@ -1,1 +1,8 @@
-st.write("jbm,,")
+import streamlit as st
+
+def main():
+    st.title("Анализ данных & Машинное обучение")
+    st.subheader("Выберите файл с данными для анализа")
+
+if __name__=='__main__':
+    main()
